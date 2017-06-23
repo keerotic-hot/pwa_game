7bd4dc3a1a1bf11951130fa2d2a329e8202d43d9
kee <keerotic@gmail.com>
2017-06-23 22:02:10 +0700

    add firebase add icon images

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+var dataCacheName = 'pwagame';
+var cacheName = 'pwagame';
+var filesToCache = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
